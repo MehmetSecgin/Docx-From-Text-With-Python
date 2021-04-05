b0ac1d45e4359bf6a8ec9c753314216bf6c625c7
Mehmet Secgin <mesecg@ttu.ee>
2021-04-05 11:33:09 +0300

    ITS ALIIVEEEEE

--- word.py
+++ word.py
@@ -1,8 +1,7 @@
 import re
 
 from docx import Document
-from docx.enum.style import WD_STYLE_TYPE
-from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
+from docx.enum.text import WD_PARAGRAPH_ALIGNMENT, WD_BREAK
 from docx.shared import Inches, Pt, RGBColor
 
 document = Document()
@@ -27,10 +26,10 @@ def writedocx(content='', font_name='Times New Roman', font_size=12, font_bold=F
               font_underline=False, color=RGBColor(0, 0, 0),
               before_spacing=0, after_spacing=10, line_spacing=1.5, keep_together=True, keep_with_next=False,
               page_break_before=False,
-              widow_control=False, align='left', style='Normal'):
-    paragraph = document.add_paragraph(style=style)
-    run = paragraph.add_run(str(content))
-    print(f'Run Style: {run.style}')
+              widow_control=False, align='left', style='Normal', paragraph=document.add_paragraph()):
+    run = paragraph.add_run(text=content)
+    paragraph.style = style
+    # print(f'Run Style: {run.style}')
     font = run.font
     font.name = font_name
     font.size = Pt(font_size)
@@ -46,9 +45,7 @@ def writedocx(content='', font_name='Times New Roman', font_size=12, font_bold=F
     paragraph_format.keep_with_next = keep_with_next
     paragraph_format.page_break_before = page_break_before
     paragraph_format.widow_control = widow_control
-    if align.lower() == 'left':
-        paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
-    elif align.lower() == 'center':
+    if align.lower() == 'center':
         paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
     elif align.lower() == 'right':
         paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.RIGHT
@@ -58,67 +55,103 @@ def writedocx(content='', font_name='Times New Roman', font_size=12, font_bold=F
         paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
 
 
-with open('test_ho1.txt', 'r', encoding='utf-8') as rf:
-    bold = False
-    for line in rf:
-        x = re.findall(r"\B%[a-zA-Z][a-zA-Z]\b%", line)
-        if x and x[0] == '%BA%':
-            bold = not bold
-            continue
-        if bold:
-            print(f'BOLD {line}')
-            writedocx(content=line, font_bold=True, font_size=17, align='right')
+def makeRun(content, paragraph, font_name='Times New Roman', font_size=12, font_bold=False, font_italic=False,
+            font_underline=False, color=RGBColor(0, 0, 0), align='left', style='Normal', add_break=False):
+    paragraph.style = style
+    run = paragraph.add_run(content)
+    if add_break:
+        run.add_break(WD_BREAK.LINE_CLEAR_LEFT)
+    font = run.font
+    font.name = font_name
+    font.size = Pt(font_size)
+    font.bold = font_bold
+    font.italic = font_italic
+    font.underline = font_underline
+    font.color.rgb = color
+    if align.lower() == 'center':
+        paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+    elif align.lower() == 'right':
+        paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.RIGHT
+    elif align.lower() == 'justify':
+        paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.JUSTIFY
+    else:
+        paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
+
+
+def funcOne():
+    with open('tag.txt', 'r', encoding='utf-8') as rf:
+        paragraph = document.add_paragraph(style='Normal')
+        for line in rf:
+            x = re.findall(r"\B%[a-zA-Z][a-zA-Z]\b%", line)
+            if x:
+                if x[0] == '%BA%':
+                    print(paragraph)
+                    writedocx(content=line[4:], font_bold=True, font_size=17, align='center', paragraph=paragraph)
+                    # paragraph = document.add_paragraph(text=line[4:], style='Normal')
+                    print(paragraph)
+                    print('Başlık')
+                elif x[0] == '%LI%':
+                    paragraph = document.add_paragraph(style='List')
+                    print(paragraph)
+                    writedocx(content=line[4:], font_size=12, style='List Bullet', paragraph=paragraph)
+
+                    print(paragraph)
+                    print('Liste')
+                elif x[0] == '%PA%':
+                    writedocx(content=line[4:], font_size=12, style='Normal', paragraph=paragraph, after_spacing=0)
+
+                    # print(line)
+                    print(paragraph)
+                    print('Paragraph')
+
+
+def yazBaslik(line):
+    a = line.split('%BA%')
+    paragraph = document.add_paragraph()
+    for ba in a:
+        makeRun(ba, paragraph, font_size=15, font_bold=True, align='center', add_break=True)
+
+
+def yazListe(line):
+    a = line.split('%LI%')
+    for ba in a:
+        paragraph = document.add_paragraph(style='List Bullet')
+        makeRun(ba, paragraph, style='List Bullet')
+
+
+def yazHeading(line):
+    a = line.split('%HE%')
+    for ba in a:
+        paragraph = document.add_heading(level=2)
+        makeRun(ba, paragraph, style='Heading 2', font_bold=True)
+        print("header")
+
+
+def yazPara(line):
+    paragraph = document.add_paragraph()
+    b = line.split('%KA%')
+    for bolum in range(len(b)):
+        if bolum % 2 == 0:
+            makeRun(b[bolum], paragraph, style='Normal')
         else:
-            print(line)
-            writedocx(line)
+            makeRun(b[bolum], paragraph, font_bold=True, style='Normal')
 
-        # if x:
-        #     print(line)
-        #     rf.read()
-        #     print(line)
-        #     writedocx(content=line, style='Body Text 2', font_bold=True)
-        # else:
-        #     writedocx(line)
-        # while not x:
-        #     print(line)
-        #     break
 
-#
-# writedocx(content='Normal', font_size= 15,font_bold=True)
-# writedocx(content='Normal', font_size=12)
-# writedocx(content='Normal', font_size= 15)
-# writedocx(content='Body', style='Body Text')
-# writedocx(content='Body', style='Body Text 2')
-# writedocx(content='Body', style='Body Text 3')
-# writedocx(content='Caption', style='Caption')
-# writedocx(content='Heading', style='Heading 1')
-# writedocx(content='a', style='Heading 2')
-# writedocx(content='a', style='Heading 3')
-# writedocx(content='a', style='Heading 4')
-# writedocx(content='a', style='Heading 5')
-# writedocx(content='a', style='Heading 6')
-# writedocx(content='a', style='Heading 7')
-# writedocx(content='a', style='Heading 8')
-# writedocx(content='a', style='Heading 9')
-# writedocx(content='Intense', style='Intense Quote')
-# writedocx(content='a', style='List')
-# writedocx(content='a', style='List 2')
-# writedocx(content='a', style='List 3')
-# writedocx(content='a', style='List Bullet')
-# writedocx(content='a', style='List Bullet 2')
-# writedocx(content='a', style='List Bullet 3')
-# writedocx(content='a', style='List Continue')
-# writedocx(content='a', style='List Continue 2')
-# writedocx(content='a', style='List Continue 3')
-# writedocx(content='a', style='List Number')
-# writedocx(content='a', style='List Number 2')
-# writedocx(content='a', style='List Number 3')
-# writedocx(content='a', style='List Paragraph')
-# # writedocx(content='a', style='Macro Text')
-# writedocx(content='a', style='No Spacing')
-# writedocx(content='a', style='Quote')
-# writedocx(content='a', style='Subtitle')
-# writedocx(content='a', style='TOCHeading')
-# writedocx(content='a', style='Title')
+def funcSecond():
+    with open('test_ho1.txt ', 'r', encoding='utf-8') as rf:
+        for line in rf:
+            x = re.findall(r"\B%[a-zA-Z][a-zA-Z]\b%", line)
+            if x:
+                if x[0] == '%BA%':
+                    yazBaslik(line[4:-1])
+                elif x[0] == '%LI%':
+                    yazListe(line[4:-1])
+                elif x[0] == '%PA%':
+                    yazPara(line[4:-1])
+                elif x[0] == '%HE%':
+                    yazHeading(line[4:-1])
+
+
+funcSecond()
 
 document.save('word.docx')
